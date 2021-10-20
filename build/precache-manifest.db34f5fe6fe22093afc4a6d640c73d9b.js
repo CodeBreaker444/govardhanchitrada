6655d8ab6e9c6f6744fcb77cb0e09bf7cc1dcc77
codebreaker444 <zeroerror.444@gmail.com>
2021-10-20 11:45:17 +0200

    added

---
+++ build/precache-manifest.db34f5fe6fe22093afc4a6d640c73d9b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e93e56a5ecef0021ebd",
+    "url": "/static/css/main.b6e9d908.chunk.css"
+  },
+  {
+    "revision": "3e93e56a5ecef0021ebd",
+    "url": "/static/js/main.2914720f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fbcf80fbaf86f768afa7",
+    "url": "/static/js/2.0b8ec275.chunk.js"
+  },
+  {
+    "revision": "4b18b1f01174093e28f2462dceb1e083",
+    "url": "/static/media/b.4b18b1f0.png"
+  },
+  {
+    "revision": "07d09dd6441bc09ead9c47027cfda10f",
+    "url": "/static/media/cb-cover.07d09dd6.png"
+  },
+  {
+    "revision": "528d9751eff55ac400298a7cf1187aa6",
+    "url": "/index.html"
+  }
+];
