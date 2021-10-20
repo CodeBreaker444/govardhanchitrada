8c7495bc1d13d7bd9a4498ae4c574313317197a4
codebreaker444 <zeroerror.444@gmail.com>
2021-10-20 11:54:25 +0200

    Corrections

---
+++ build/precache-manifest.4a23e6d339b5e0db725e7cc8d696a4ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ce44fd36bb3911a721e",
+    "url": "/static/css/main.889b6eb4.chunk.css"
+  },
+  {
+    "revision": "6ce44fd36bb3911a721e",
+    "url": "/static/js/main.2914720f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fbcf80fbaf86f768afa7",
+    "url": "/static/js/2.0b8ec275.chunk.js"
+  },
+  {
+    "revision": "4b18b1f01174093e28f2462dceb1e083",
+    "url": "/static/media/b.4b18b1f0.png"
+  },
+  {
+    "revision": "07d09dd6441bc09ead9c47027cfda10f",
+    "url": "/static/media/cb-cover.07d09dd6.png"
+  },
+  {
+    "revision": "428edc6e16bfa5dd5eebd4872cb7149e",
+    "url": "/index.html"
+  }
+];
